cae44b86cb4bd6a6e16fab00a471acbe626822d1
Przemyslaw Bobrowicz <peter@bobrowicz.net>
2016-05-11 20:33:24 -0400

    script tweak
    took out bunch of if statements and created a function instead

--- script/launcher_script.py
+++ script/launcher_script.py
@@ -4,68 +4,58 @@ import subprocess
 
 BAUD_RATE = 56000
 
+# map buttons to ascii codes sent from microcontroller
+# b"A" - button 0
+# b"B" - button 1
+# b"C" - button 2
+# b"D" - button 3
+buttons = {b"A":0, b"B":1, b"C":2, b"D":4}
+
 # ask user to sepecify serial port to listen to
 serial_port = input("Serial port: ")
 
 # open text file containing action shortcuts
-in_file = open("actions.txt")
+in_file = open("shortcuts.txt")
 
 # read file into a list
-buttons = []
+shortcuts = []
 for line in in_file:
     if (line[0] != '#'):
-        buttons.append(line)
+        shortcuts.append(line)
 
 in_file.close
 
-
-serial_in = serial.Serial(serial_port, BAUD_RATE, timeout = 5)
+# set up serial interface
+serial_in = serial.Serial(serial_port, BAUD_RATE, timeout = 10)
 serial_in.flush()
 
 print("Listening")
 
+def action(code):
+    try:
+        # if first character in the shortcut string is an 'h' 
+        # then it probably stands for http://somethin...   
+        if (shortcuts[code][0] == 'h'):
+            webbrowser.open(shortcuts[code]) 
+        # otherwise it's probably a directory
+        else:
+            subprocess.call([shortcuts[code]]) 
+    except:
+        print("Error. Button probably unassigned.")
+
 #continously check serial interface for incoming data
 while(1):    
     # read in one byte
-    response = serial_in.read(1)    
-    # print out received byte
-    print(response)
+    ascii_code = serial_in.read(1)    
     
     # b"O" - hearbeat
-    # b"A" - button 1
-    # b"B" - button 2
-    # b"C" - button 3
-    # b"D" - button 4
-    
-    if response == b"O":
+    if ascii_code == b"O":
         print("Got OK Byte.")
         
-    elif response == b"A":
-        #check first character of the string assigned to the button
-        #letter h implies http therefore a web site
-        #for any other character assume a directory
-        if (buttons[0][0] == 'h'):
-            webbrowser.open(buttons[0]) 
-        else:
-            subprocess.call([buttons[0]])
-            
-    elif response == b"B":
-        if (buttons[1][0] == 'h'):
-            webbrowser.open(buttons[1]) 
-        else:
-            subprocess.call([buttons[1]])  
-            
-    elif response == b"C":
-        if (buttons[2][0] == 'h'):
-            webbrowser.open(buttons[2]) 
-        else:
-            subprocess.call([buttons[2]])  
-            
-    elif response == b"D":
-        if (buttons[3][0] == 'h'):
-            webbrowser.open(buttons[3]) 
-        else:
-            subprocess.call([buttons[3]])
+    # if code representing one of the buttons comes in then
+    # pass assigned button number to the function
+    elif ascii_code in buttons:
+        action(buttons[ascii_code])
             
     else:
         print("Got nothing.  Still waiting.")
